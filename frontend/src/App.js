339d1a75f43de8692089833b87b4fbb298407898
Lorenalgm <lorena@proesc.com>
2020-08-04 19:16:30 -0300

    Criada estrutura html da landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,130 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="hero">
+        <div className="header">
+          <img src="" alt="Logo" />
+          <div className="date">1 a 5 de janeiro</div>
+          <div className="free">100% online e gratuito!</div>
+          <div className="new-content">Conteúdo inédito!</div>
+        </div>
+        <div className="hero-content">
+          <h1>Avance para o próximo nível</h1>
+          <p>Um evento para dar o próximo passo na sua evolução em programação.</p>
+          <a href="#">QUERO PARTICIPAR</a>
+        </div>
+      </div>
+      <div className="about">
+        <img src="" alt="Logo" />
+        <div className="about-content">
+          <h1>O que é a Next Level Week?</h1>
+          <p className="highlight">O NLW é um evento com muito código, desafios, 
+            networking e um único objetivo: te levar para o próximo nível.</p>
+          <p>Programação é uma disciplina prática. Ou seja, para dominar uma 
+            tecnologia não basta apenas conhecer a teoria ou ler a documentação: você precisa 
+            colocar a mão no código.</p>
+          <p>Através do nosso método você vai aprender novas ferramentas, conhecer novas tecnologias 
+            e descobrir hacks que vão impulsionar a sua carreira.</p>
+          <p>Um experiência online e totalmente gratuita para evoluir suas habilidades em programação
+            e colocar mais um trabalho completo no seu portfolio.</p> 
+        </div>
+      </div>
+      <div className="pillars-section">
+        <img src="" alt="Icon" />
+        <h1>Nosso método é baseado em 3 pilares</h1>
+        <div className="pillars">
+          <div className="pillar">
+            <h1>Prática</h1>
+            <p>Existem muitas formas de estudar programação, mas só existe uma forma
+              de aprender: na prática. A teoria só funciona se você souber onde e quando aplicar, 
+              e são as horas de código que vão te ensinar a tomar as melhores decisões.</p>
+          </div>
+          <div className="pillar">
+            <h1>Foco</h1>
+            <p>Programação é um universo infinito, por isso ter foco é o primeiro passo 
+              para atingir seus objetivos. Saber onde você quer chegar e não se distrair com aquilo que 
+              não faz sentido para o seu momento, é a melhor forma de acelerar sua evolução.</p>
+          </div>
+          <div className="pillar">
+            <h1>Grupo</h1>
+            <p>Para se destacar é preciso ter habilidades que vão além do código, 
+              e a melhor forma de dominar essas habilidades também é na prática. 
+              Ajudar outros devs, se posicionar e receber feedback formam o bom profissional.</p>
+          </div>
+        </div>
+      </div>
+      <div className="power-section">
+        <h1>O poder dos 3 pilares combinados</h1>
+        <div className="powers">
+          <div className="power">
+            <h1>01</h1>
+            <h2>Experiência e portfolio</h2>
+          </div>
+          <div className="power">
+            <h1>01</h1>
+            <h2>Experiência e portfolio</h2>
+          </div>
+          <div className="power">
+            <h1>01</h1>
+            <h2>Experiência e portfolio</h2>
+          </div>
+          <div className="power">
+            <h1>01</h1>
+            <h2>Experiência e portfolio</h2>
+          </div>
+          <div className="power">
+            <h1>01</h1>
+            <h2>Experiência e portfolio</h2>
+          </div>
+          <div className="power">
+            <h1>01</h1>
+            <h2>Experiência e portfolio</h2>
+          </div>
+          <div className="power">
+            <h1>01</h1>
+            <h2>Experiência e portfolio</h2>
+          </div>
+          <div className="power">
+            <h1>01</h1>
+            <h2>Experiência e portfolio</h2>
+          </div>
+          <div className="power">
+            <h1>01</h1>
+            <h2>Experiência e portfolio</h2>
+          </div>
+        </div>
+      </div>
+      <div className="company">
+        <h1>Sobre a Rocketseat</h1>
+        <div className="company-numbers">
+          <div className="number">
+            <h1>12 mil</h1>
+            <p>Alunos treinados nos bootcamps</p>
+          </div>
+          <div className="number">
+            <h1>12 mil</h1>
+            <p>Alunos treinados nos bootcamps</p>
+          </div>
+          <div className="number">
+            <h1>12 mil</h1>
+            <p>Alunos treinados nos bootcamps</p>
+          </div>
+          <div className="number">
+            <h1>12 mil</h1>
+            <p>Alunos treinados nos bootcamps</p>
+          </div>
+        </div>
+      </div>
+      <div className="disclaimer">
+        <img src="" alt="Icon" />
+        <div className="disclaimer-content">
+          <p>Ao final desse evento nós abrimos as 
+            inscrições para novas turmas dos nossos bootcamps pagos.</p>
+        </div>
+      </div>
     </div>
   );
 }
